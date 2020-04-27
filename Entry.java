96ec4f1c5de59c581197e644cfe76f6334a993cf
Coguox7 <53919007+Coguox7@users.noreply.github.com>
2020-04-26 20:52:07 -0600

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,43 @@
+//Javier Hernández 19202
+//Pablo Coguox 19960
+// Extraido de https://www.sanfoundry.com/java-program-implement-splay-tree/
+
+public class Entry implements Comparable<Entry> {
+    private String key;
+    private String value;
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "key=" + key +
+                ", value='" + value + '\'' +
+                '}';
+    }
+
+    public Entry(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+
+    @Override
+    public int compareTo(Entry that) {
+        if (this.key.compareTo(that.key) < 0) {
+            return 1;
+        } else if (this.key.compareTo(that.key) > 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+
+}
